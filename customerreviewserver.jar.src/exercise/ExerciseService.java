03b38895d2bf4199d0810cebd765467b91095762
ming <m1n9j0hn@gmail.com>
2018-03-09 14:12:11 -0500

    test commit

--- customerreviewserver.jar.src/exercise/ExerciseService.java
+++ customerreviewserver.jar.src/exercise/ExerciseService.java
@@ -1,4 +1,11 @@
 package exercise;
+import de.hybris.platform.customerreview.*;
 
-public class ExerciseService {
+public interface ExerciseService {
+
+    Integer getNumberOfReviewsByRange(ProductModel paramProductModel, Double leftRange, Double rightRange);
+
+    CustomerReviewModel createCheckedCustomerReview(Double paramDouble, String paramString1, String paramString2,
+                                                    UserModel paramUserModel, ProductModel paramProductModel,
+                                                    String cwFilePath) throws JaloInvalidParameterException;
 }
